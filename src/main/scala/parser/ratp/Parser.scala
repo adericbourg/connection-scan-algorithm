6432594838d2f2a34b5d58ec0c5e583bc30199bd
Alban Dericbourg <alban@dericbourg.net>
2015-08-30 15:28:40 +0200

    Display some random trip

--- src/main/scala/parser/ratp/Parser.scala
+++ src/main/scala/parser/ratp/Parser.scala
@@ -11,5 +11,14 @@ object Parser {
     val stops = CSVReader.open(new File("/home/alban/tmp/lines/ligne9/stops.txt")).allWithHeaders().map(Stop.parse)
     val stopTimes = CSVReader.open(new File("/home/alban/tmp/lines/ligne9/stop_times.txt")).allWithHeaders().map(StopTime.parse)
     val transfers = CSVReader.open(new File("/home/alban/tmp/lines/ligne9/transfers.txt")).allWithHeaders().map(Transfer.parse)
+
+    val stopTimesByTripId: Map[Long, List[StopTime]] = stopTimes.groupBy(_.tripId)
+    val stopsByStopId: Map[Long, List[Stop]] = stops.groupBy(_.stopId)
+
+    // Display some random trip
+    val trip: Trip = trips.head
+    stopTimesByTripId.get(trip.tripId).get.foreach(stopTime =>
+      stopsByStopId.getOrElse(stopTime.stopId, List()).foreach(stop => println(s"${stop.stopName} : ${stopTime.arrivalTime} : ${stopTime.departureTime}"))
+    )
   }
 }
