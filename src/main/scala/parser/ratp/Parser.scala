6b8ac69f2db19bfe0310247dc57856a31620dc29
Alban Dericbourg <alban@dericbourg.net>
2015-09-12 11:13:55 +0200

    Let's go from Miromesnil to Grands Boulevards

--- src/main/scala/parser/ratp/Parser.scala
+++ src/main/scala/parser/ratp/Parser.scala
@@ -1,8 +1,12 @@
 package parser.ratp
 
 import java.io.File
+import java.time.{LocalDate, LocalTime, ZoneOffset}
 
+import algo.{CSA, Connection, Timetable}
 import com.github.tototoshi.csv._
+
+import scala.annotation.tailrec
 
 object Parser {
   def main(args: Array[String]) {
@@ -15,10 +19,40 @@ object Parser {
     val stopTimesByTripId: Map[Long, List[StopTime]] = stopTimes.groupBy(_.tripId)
     val stopsByStopId: Map[Long, List[Stop]] = stops.groupBy(_.stopId)
 
+
+
     // Display some random trip
     val trip: Trip = trips.head
     stopTimesByTripId.get(trip.tripId).get.foreach(stopTime =>
-      stopsByStopId.getOrElse(stopTime.stopId, List()).foreach(stop => println(s"${stop.stopName} : ${stopTime.arrivalTime} : ${stopTime.departureTime}"))
+      stopsByStopId.getOrElse(stopTime.stopId, List()).foreach(stop => println(s"${stop.stopName} (${stop.stopId}) : ${stopTime.arrivalTime} : ${stopTime.departureTime}"))
     )
+
+    // Display connections for that trip
+    val connections: Seq[Connection] = stopTimesToConnections(stopTimesByTripId.get(trip.tripId).get)
+    connections.foreach(connection =>
+      println(s"${connection.departureStation} (@${connection.departureTimestamp})-> ${connection.arrivalStation} (@${connection.arrivalTimestamp})")
+    )
+
+    // Build timetable... and go from Miromesnil to Grands Boulevards
+    val timetable: Timetable = Timetable(connections)
+    CSA(timetable).compute(1819, 1712, 1442054940)
+  }
+
+  private def stopTimesToConnections(stopTimes: Seq[StopTime]): Seq[Connection] = {
+    @tailrec
+    def loop(times: Seq[StopTime], connections: List[Connection]): List[Connection] = {
+      times match {
+        case Nil => connections
+        case head :: Nil => connections
+        case head :: tail =>
+          val departureStop = head.stopId.toInt
+          val arrivalStop = tail.head.stopId.toInt
+          val departureTime = LocalDate.now().atTime(LocalTime.MIDNIGHT).plusSeconds(head.departureTime.getSeconds).toInstant(ZoneOffset.UTC).getEpochSecond.toInt
+          val arrivalTime = LocalDate.now().atTime(LocalTime.MIDNIGHT).plusSeconds(tail.head.departureTime.getSeconds).toInstant(ZoneOffset.UTC).getEpochSecond.toInt
+          val connection: Connection = Connection(departureStop, arrivalStop, departureTime, arrivalTime)
+          loop(tail, connections :+ connection)
+      }
+    }
+    loop(stopTimes, List())
   }
 }
